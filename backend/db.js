8632a5367423e00c723d52a9dfb046ede0f0cb88
CaesarAidarus22 <muh.caesar@mhs.usk.ac.id>
2026-05-18 13:27:40 +0700

    fix: hapus password

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2'); // 🔥 WAJIB ADA
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'lurimamirdas',
+  password: '',
   database: 'foodwaste'
 });
 
